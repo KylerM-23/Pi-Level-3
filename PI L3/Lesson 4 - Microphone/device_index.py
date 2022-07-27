ebcd80971818ab4df65356046220f950f9798a09
KylerM-23 <49105712+KylerM-23@users.noreply.github.com>
2022-07-26 18:11:12 -0700

    Delete PI L3 directory

--- PI L3/Lesson 4 - Microphone/device_index.py
+++
@@ -1,9 +0,0 @@
-import pyaudio
-
-p = pyaudio.PyAudio() #pyaudio object
-
-for device in range (p.get_device_count()):
-    print("Index", device, ":", p.get_device_info_by_index(device).get('name'))
-
-    
-    
